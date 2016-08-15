e60c6504c391741533b3fc0e06cb10004b284bbf
Giancarlo Vera <paulterry2001@hotmail.com>
2016-08-15 02:41:15 -0300

    Add circle animations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HTML5 Canvas
 A very basic example of Canvas API, just for fun!.
+
+Important: If you navigate through the tags, please ignore the 'step03' and go ahead from step02 to step04, because the commit of step03 was made from step02 (not from master), so the push was not recorded. It's my first time working with tags and I've made this mistake.
--- js/app.js
+++ js/app.js
@@ -14,10 +14,24 @@
 			posX += 1;
 			posY += 1;
 
-			ctx.clearRect(0,0,box.width,box.height);
-			ctx.fillStyle = 'green';
-			ctx.fillRect(posX,0,60,50);
-		},50);
+			ctx.fillStyle = '#1a1a1b';
+			ctx.fillRect(0,0,box.width,box.height);
+
+			ctx.fillStyle = '#fff';
+			ctx.beginPath();
+			ctx.arc(posX,120,18,0,Math.PI*2);
+			ctx.fill();
+
+			ctx.fillStyle = '#f22cb3';
+			ctx.beginPath();
+			ctx.arc(90,(posY-20),24,0,Math.PI*2);
+			ctx.fill();
+
+			ctx.fillStyle = '#f27b2c';
+			ctx.beginPath();
+			ctx.arc(210,(posY+60),12,0,Math.PI*2);
+			ctx.fill();
+		},20);
 	}
 
 	window.onload = initCanvas;
