a11f2fd95762ace79adef5143a3aab7e5353e535
Giancarlo Vera <paulterry2001@hotmail.com>
2016-08-15 01:11:48 -0300

    Add first function declarations

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+(function(){
+	var box;
+	var ctx;
 
+	function initCanvas(){
+		box = document.getElementById('myCanvas');
+		ctx = box.getContext('2d');
+		initDraw();
+	}
+	function initDraw(){
+		var posX = 0;
+		var posY = 0;
+		setInterval(function(){
+
+		},100);
+	}
+
+	window.onload = initCanvas;
+
+})();
