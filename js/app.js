fa55ff983e53d6bf45f4b3f8f061e33f26595805
Giancarlo Vera <paulterry2001@hotmail.com>
2016-08-15 05:44:28 -0300

    Add the UI for our Canvas Game

--- js/app.js
+++ js/app.js
@@ -1,39 +1,8 @@
-(function(){
-	var box;
-	var ctx;
+$(document).ready(function(){
+	
 
-	function initCanvas(){
-		box = document.getElementById('myCanvas');
-		ctx = box.getContext('2d');
-		initDraw();
-	}
-	function initDraw(){
-		var posX = 0;
-		var posY = 0;
-		setInterval(function(){
-			posX += 1;
-			posY += 1;
+});
 
-			ctx.fillStyle = '#1a1a1b';
-			ctx.fillRect(0,0,box.width,box.height);
 
-			ctx.fillStyle = '#fff';
-			ctx.beginPath();
-			ctx.arc(posX,120,18,0,Math.PI*2);
-			ctx.fill();
 
-			ctx.fillStyle = '#f22cb3';
-			ctx.beginPath();
-			ctx.arc(90,(posY-20),24,0,Math.PI*2);
-			ctx.fill();
 
-			ctx.fillStyle = '#f27b2c';
-			ctx.beginPath();
-			ctx.arc(210,(posY+60),12,0,Math.PI*2);
-			ctx.fill();
-		},20);
-	}
-
-	window.onload = initCanvas;
-
-})();
