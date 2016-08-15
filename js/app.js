3989f40d3c015216f614b7bb1c4c578152f40e7a
Giancarlo Vera <paulterry2001@hotmail.com>
2016-08-15 01:42:49 -0300

    Add green square animation

--- js/app.js
+++ js/app.js
@@ -11,8 +11,13 @@
 		var posX = 0;
 		var posY = 0;
 		setInterval(function(){
+			posX += 1;
+			posY += 1;
 
-		},100);
+			ctx.clearRect(0,0,box.width,box.height);
+			ctx.fillStyle = 'green';
+			ctx.fillRect(posX,0,60,50);
+		},50);
 	}
 
 	window.onload = initCanvas;
