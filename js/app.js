44ec323af20642c2e3c0b35f391bffda462eec7c
Giancarlo Vera <paulterry2001@hotmail.com>
2016-08-15 22:59:58 -0300

    Add keyboard controller & score display/reset

--- js/app.js
+++ js/app.js
@@ -1,108 +1,143 @@
 $(document).ready(function(){
-	//Variables
-	var canvas = $('#myCanvas')[0];
-	var ctx = canvas.getContext('2d');
-	var w = $('#myCanvas').width();
-	var h = $('#myCanvas').height();
-	var cw = 8;
-	var d = 'right';
+	//Define Vars
+	var canvas = $("#myCanvas")[0];
+	var ctx = canvas.getContext("2d");
+	var w = $("#myCanvas").width();
+	var h = $("#myCanvas").height();
+	var cw = 15;
+	var d = "right";
 	var food;
 	var score;
-	var color = 'green';
+	var color ="blue";
 	var speed = 100;
 
+	//Snake Array
 	var snake_array;
 
-	//Game Initializer
+	//Initializer
 	function init(){
 		d = "right";
-		createSnake();
-		createFood();
+		create_snake();
+		create_food();
 		score = 0;
-
-		if(typeof game_loop != 'undefined') clearInterval(game_loop);
-		game_loop = setInterval(paint,speed);
+		
+		//Use a timer to call paint function
+		if(typeof game_loop != "undefined") clearInterval(game_loop);
+		game_loop = setInterval(paint, speed);
 	}
+	
 	init();
-
+	
 	//Create Snake
-	function createSnake () {
+	function create_snake(){
 		var length = 5;
-		snake_array = [];
-		for (var i=length-1; i>=0; i--) {
-			snake_array.push({x: i, y: 0});
-		};
+		snake_array = []; 
+		for(var i = length-1; i>=0; i--){
+			snake_array.push({x: i, y:0});
+		}
 	}
-
+	
 	//Create Food
-	function createFood() {
+	function create_food(){
 		food = {
-			x: Math.round(Math.random()*(w-cw)/cw),
-			y: Math.round(Math.random()*(h-cw)/cw),
+			x: Math.round(Math.random()*(w-cw)/cw), 
+			y: Math.round(Math.random()*(h-cw)/cw), 
 		};
 	}
-
+	
 	//Paint Snake
 	function paint(){
 		//Paint The Canvas
-		ctx.fillStyle = 'black';
-		ctx.fillRect(0,0,w,h);
-		ctx.strokeStyle = 'white';
-		ctx.strokeRect(0,0,w,h);
-
+		ctx.fillStyle = "black";
+		ctx.fillRect(0, 0, w, h);
+	
 		var nx = snake_array[0].x;
 		var ny = snake_array[0].y;
-
-		if(d == 'right') nx++;
-		else if(d == 'left') nx--;
-		else if(d == 'up') ny--;
-		else if(d == 'down') ny--;
-
-		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || checkCollision(nx,ny,snake_array)){
-			init();
-
+		
+		if(d == "right") nx++;
+		else if(d == "left") nx--;
+		else if(d == "up") ny--;
+		else if(d == "down") ny++;
+		
+		//Collide code
+		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array)){			
+			//init();
+			//Insert Final Score
+			$('#finalScore').html(score);
+			//Show Overlay
+			$('#overlay').fadeIn(300);
 			return;
 		}
+		
 		if(nx == food.x && ny == food.y){
 			var tail = {x: nx, y: ny};
 			score++;
-
-			createFood();
-		}else{
+			//Create Food
+			create_food();
+		} else {
 			var tail = snake_array.pop();
 			tail.x = nx; tail.y = ny;
 		}
+		
 		snake_array.unshift(tail);
-
-		for(var i=0; i<snake_array.length; i++){
+		
+		for(var i = 0;i < snake_array.length;i++){
 			var c = snake_array[i];
-			paintCell(c.x,c.y);
+			paint_cell(c.x,c.y);
 		}
-
-		paintCell(food.x,food.y);
-
-		//checkScore(score);
+		
+		//Paint Cell
+		paint_cell(food.x,food.y);
+		
+		//Check Score
+		checkscore(score);
+		
+		//Display Current Score
+		$('#score').html('Your Score: '+score);
 	}
-
-	//Paint cw
-	function paintCell(x,y){
-		ctx.fillStyle = color;
+	
+	function paint_cell(x,y){
+		ctx.fillStyle=color;
 		ctx.fillRect(x*cw,y*cw,cw,cw);
-		ctx.strokeStyle = 'white';
+		ctx.strokeStyle="white";
 		ctx.strokeRect(x*cw,y*cw,cw,cw);
 	}
-
-	//Check Collissions
-	function checkCollision(x,y,array){
-		for(var i=0; i<array.length; i++){
-			if(array[i.x == x && array[i].y == y])
+	
+	function check_collision(x, y, array){
+		for(var i = 0;i < array.length;i++){
+			if(array[i.x == x && array[i].y ==y])
 				return true;
 		}
 		return false;
 	}
-
+	
+	function checkscore(score){
+		if(localStorage.getItem('highscore') === null){
+			//If there is no high score
+			localStorage.setItem('highscore',score);
+		} else {
+			//If there is a high score
+			if(score > localStorage.getItem('highscore')){
+				localStorage.setItem('highscore',score);
+			}
+		}
+		
+		$('#high_score').html('High Score: '+localStorage.highscore);
+	}
+	
+	//Keyboard Controller
+	$(document).keydown(function(e){
+		var key = e.which;
+		if(key == "37" && d !="right") d  = "left";
+		else if(key == "38" && d != "down") d = "up";
+		else if(key == "39" && d != "left") d = "right";
+		else if(key == "40" && d != "up") d = "down";
+	});
 });
 
-
-
-
+function resetScore(){
+	localStorage.highscore = 0;
+	//Display High Score
+	highscorediv = document.getElementById('high_score');
+	highscorediv.innerHTML ='High Score: 0';
+}
